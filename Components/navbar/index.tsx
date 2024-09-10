91927b6bf482b4313c15d84d9427542bd573b499
AbbasovRasim <ras.abbasov@gmail.com>
2024-09-10 05:41:16 -0700

    commit

--- Components/navbar/index.tsx
+++ Components/navbar/index.tsx
@@ -1,40 +1,67 @@
 "use client";
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 import Link from "next/link";
+import styles from "./style.module.css";
+const Navbar = () => {
+  const [isActive, setIsActive] = useState(false);
 
-export const Navbar = () => {
-  const [isHovered, setIsHovered] = useState(false);
-
-  const handleMouseOver = () => {
-    setIsHovered(true);
+  const handleClick = () => {
+    setIsActive(true);
   };
 
-  const handleMouseOut = () => {
-    setIsHovered(false);
+  const handleBlur = () => {
+    setIsActive(false);
   };
 
   return (
     <section className="h-[70vh] flex items-center justify-center bg-[url(/pictures/bcfoto.png)] bg-no-repeat bg-cover">
       <div>
         <div className="flex justify-center bg-white gap-4  py-2 px-20  border  rounded-full picture">
-          <div>
-            {" "}
-            <Link
-              onMouseMove={handleMouseOver}
-              onMouseOut={handleMouseOut}
-              style={{ border: isHovered ? "1px solid green " : "none" }}
-              href="/about"
-            >
-              About
-            </Link>
-          </div>
-          <Link href="/menue">Menue</Link>
-          <Link href="/beverages">Beverages</Link>
-          <Link href="/delivery">Delivery</Link>
-          <Link href="/contact">Contact us</Link>
+          <Link
+            c
+            className={isActive ? "underline" : ""}
+            onMouseOver={handleClick}
+            onMouseLeave={handleBlur}
+            href="/about"
+          >
+            About
+          </Link>
+          <Link
+            className={isActive ? "underline" : ""}
+            onMouseOver={handleClick}
+            onMouseLeave={handleBlur}
+            href="/menue"
+          >
+            Menue
+          </Link>
+          <Link
+            className={isActive ? "underline" : ""}
+            onMouseOver={handleClick}
+            onMouseLeave={handleBlur}
+            href="/beverages"
+          >
+            Beverages
+          </Link>
+          <Link
+            className={isActive ? "underline" : ""}
+            onMouseOver={handleClick}
+            onMouseLeave={handleBlur}
+            href="/delivery"
+          >
+            Delivery
+          </Link>
+          <Link
+            className={isActive ? "underline  " : ""}
+            onMouseOver={handleClick}
+            onMouseLeave={handleBlur}
+            href="/contact"
+          >
+            Contact us
+          </Link>
         </div>
       </div>
     </section>
   );
 };
+
+export default Navbar;
